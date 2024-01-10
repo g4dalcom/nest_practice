ab587f9fbb010f6a3dce9a9055b3a81638c3d13e
g4dalcom <g4dalcom@gmail.com>
2024-01-10 12:09:59 +0900

    feat: user controller, service 기본 동작 확인

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  getAllUsers(): string {
+    return '옛다! users info';
+  }
+}
