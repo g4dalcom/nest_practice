ca0e4aefa650d48b27101777a6a35c6393f473ef
g4dalcom <g4dalcom@gmail.com>
2024-01-12 10:52:07 +0900

    feat: read API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/request.dto';
 import { ResponseDto } from './dto/reponse.dto';
@@ -10,5 +10,15 @@ export class UserController {
   @Post()
   createUser(@Body() requestDto: CreateUserDto): Promise<ResponseDto> {
     return this.userService.createUser(requestDto);
+  }
+
+  @Get('/all')
+  getUsers(): Promise<ResponseDto[]> {
+    return this.userService.getUsers();
+  }
+
+  @Get('/:id')
+  getUser(@Param('id') id: string): Promise<ResponseDto> {
+    return this.userService.getUser(Number(id));
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Body, Injectable } from '@nestjs/common';
+import { Body, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { CreateUserDto } from './dto/request.dto';
@@ -14,10 +14,26 @@ export class UserService {
 
   async createUser(@Body() requestDto: CreateUserDto): Promise<ResponseDto> {
     const user = this.userRepository.create(requestDto);
-    this.userRepository.save(user);
+    await this.userRepository.save(user);
 
     const response = { ...user, role: UserRole.GHOST };
 
     return response;
+  }
+
+  async getUsers(): Promise<ResponseDto[]> {
+    const user = await this.userRepository.find();
+
+    return user;
+  }
+
+  async getUser(id: number): Promise<ResponseDto> {
+    const user = await this.userRepository.findOneBy({ id: id });
+
+    if (!user) {
+      throw new NotFoundException(`${id} 회원은 존재하지 않습니다.`);
+    }
+
+    return user;
   }
 }
